c73812e66fe0fea042bcbd3c1021a3f1f295d974
antalszava <antalszava@gmail.com>
2025-05-27 13:24:27 +0200

    Steane code from Nielsen & Chuang

---
+++ steane_code/example.py
@@ -0,0 +1,80 @@
+# Steane code example
+#
+# The quantum circuit in this example includes measurements of the generators
+# of the Steane code, to give the error syndrome.
+#
+# The circuit is built as shown in Nielsen & Chuang's Quantum Computation and
+# Quantum Information publication:
+#
+# Michael A. Nielsen; Isaac L. Chuang (9 December 2010). "4.4 Measurement".
+# Quantum Computation and Quantum Information: 10th Anniversary Edition.
+# Cambridge University Press. p. 186. ISBN 978-1-139-49548-6.
+#
+
+# importing Qiskit
+from qiskit_aer import AerSimulator
+from qiskit import QuantumCircuit, transpile, ClassicalRegister, QuantumRegister
+
+def steane_code_circuit(code_quantum_register):
+    """Quantum circuit for measuring the generators of the Steane code.
+
+    The measurements produce the the error syndrome.
+
+    The top six qubits are the ancilla used for the measurement, and the bottom
+    seven are the code qubits.
+
+    Args:
+        code_quantum_register(QuantumRegister): Quantum register referencing
+            the code qubits.
+
+    Returns:
+        QuantumCircuit: Steane code circuit with measurements.
+    """
+    qr_aux = QuantumRegister(6, "aux")
+    cr_aux = ClassicalRegister(6, "results")
+    qc = QuantumCircuit(qr_aux,code_quantum_register, cr_aux)
+
+    for qubit in qr_aux:
+        qc.reset(qubit)
+
+    qc.barrier()
+
+    for qubit in qr_aux:
+        qc.h(qubit)
+
+    qc.barrier()
+
+    qc.cx(qr_aux[0],code_quantum_register[[0, 4, 5, 6]])
+    qc.barrier()
+    qc.cx(qr_aux[1],code_quantum_register[[1, 3, 5, 6]])
+    qc.barrier()
+    qc.cx(qr_aux[2],code_quantum_register[[2, 3, 4, 5]])
+    qc.barrier()
+
+    qc.cz(qr_aux[3],code_quantum_register[[0, 2, 3, 6]])
+    qc.barrier()
+    qc.cz(qr_aux[4],code_quantum_register[[1, 2, 4, 6]])
+    qc.barrier()
+    qc.cz(qr_aux[5],code_quantum_register[[0, 1, 2, 5]])
+    qc.barrier()
+
+    for qubit in qr_aux:
+        qc.h(qubit)
+
+    for idx, qubit in enumerate(qr_aux):
+        qc.measure(qubit, cr_aux[idx])
+
+    return qc
+
+code_qr = QuantumRegister(7, "code")
+circuit = steane_code_circuit(code_qr)
+
+# use local simulator
+simulator = AerSimulator()
+sim_job = simulator.run(circuit, shots=1000)
+result = sim_job.result()
+counts = result.get_counts()
+print(counts)
+
+# Uncomment the following line to draw the circuit:
+# print(circuit.draw())
